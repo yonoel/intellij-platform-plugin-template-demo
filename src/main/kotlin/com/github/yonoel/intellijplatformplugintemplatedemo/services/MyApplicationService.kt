76acb87187332b3aec368751785926f391509b9e
GitHub Action <action@github.com>
2022-04-25 09:48:40 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/yonoel/intellijplatformplugintemplatedemo/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.yonoel.intellijplatformplugintemplatedemo.services
+
+import com.github.yonoel.intellijplatformplugintemplatedemo.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
